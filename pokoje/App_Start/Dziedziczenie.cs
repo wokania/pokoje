8a72619487082c5f0d270df95610bd166f65cc10
Dominik <Dominik@DOMINIK_KOM>
2016-10-20 19:53:53 +0200

    Dziedziczenie

---
+++ pokoje/App_Start/Dziedziczenie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokoje.App_Start
+{
+    public class Dziedziczenie
+    {
+        public string aa = "Hello";
+        void metoda()
+        {
+            Console.WriteLine(aa);
+        }
+    }
+    public class Dziecko : Dziedziczenie
+    {
+        Dziedziczenie aa1 = new Dziedziczenie();
+        void metoda1()
+        {
+            aa1.aa.ToString();
+        }
+    }
+}
