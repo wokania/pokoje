03866039584686d8d919c235a64b8e0eecacd525
Dominik <Dominik@DOMINIK_KOM>
2016-10-25 19:34:31 +0200

    UStawienie routingu

--- pokoje/App_Start/RouteConfig.cs
+++ pokoje/App_Start/RouteConfig.cs
@@ -12,18 +12,19 @@ namespace pokoje
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                name: "Calendar",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Calendar", action="Index", id = UrlParameter.Optional }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
 
-            routes.MapRoute(
-                name: "Calendar",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Calendar", action = "Calendar", id = UrlParameter.Optional }
-            );
-        }
+
+        }      
     }
 }
--- pokoje/Controllers/CalendarController.cs
+++ pokoje/Controllers/CalendarController.cs
@@ -104,5 +104,6 @@ namespace pokoje.Controllers
             return (ContentResult)new AjaxSaveResponse(action);
         }
     }
+
 }
 
