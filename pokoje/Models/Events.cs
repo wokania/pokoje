cc13027beda80da883ad57f94d2bb2a1471255b7
wojtek <wokania@onet.eu>
2016-11-18 21:28:46 +0100

    dodano bazę opartą o code first sprawdzono na przykładzie tabeli Events

---
+++ pokoje/Models/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokoje.Models
+{
+    public class Events
+    {
+        public Events() { }
+        public int EventId { get; set; }
+        public string content { get; set; }
+        public DateTime? startDate { get; set; }
+        public DateTime? endDate { get; set; }
+        public ICollection<ApplicationUserManager> user { get; set; }
+        public ICollection<Rooms> roomId { get; set; }
+    }
+}
---
+++ pokoje/Models/Rooms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokoje.Models
+{
+    public class Rooms
+    {
+        public Rooms() { }
+        public int room_id { get; set; }
+        public string roomColor { get; set; }
+
+        public ICollection<Events> Event { get; set; }
+    }
+}
